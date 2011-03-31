0ad6e67aac374e6b0c041bf52275bddb51d994a7
Pierre Marchand <pierremarc07@gmail.com>
2011-03-31 10:23:48 +0200

    Debug pattern size

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -36,6 +36,7 @@
 #include <QFileInfo>
 #include <QTransform>
 #include <QMessageBox>
+#include <QDebug>
 
 
 MainWindow * MainWindow::MWInstance = 0;
@@ -253,6 +254,7 @@ void MainWindow::slotSelectPattern()
 		tp = tpc->value(pn);
 	currentRug->setThreadPattern(tp);
 	QImage im(tp->getImage());
+	qDebug()<<"SlotSP"<<im.width()<<im.widthMM()<<im.dotsPerMeterX();
 	ui->rugWidth->setValue( im.widthMM() / 2.0 );
 	ui->patternSize->setValue(im.widthMM());
 
