7cc0600c2e803ef4da8924f5041f42ce51c89cc1
Pierre Marchand <pierre.m@atelier-cartographique.be>
2025-04-02 08:04:00 +0200

    minimal changes to get it working with qt5

--- FuzzyRug.pro
+++ FuzzyRug.pro
@@ -4,7 +4,7 @@
 #
 #-------------------------------------------------
 
-QT       += core gui
+QT       +=  widgets printsupport
 
 TARGET = FuzzyRug
 TEMPLATE = app
--- main.cpp
+++ main.cpp
@@ -18,8 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-
-#include <QtGui/QApplication>
+#include <QApplication>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
